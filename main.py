2dc3bcfa1e46a224f7135e3d3dc886279bc93bcd
aruna-makaruna14 <arun4ik14@gmail.com>
2024-09-16 17:56:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+print((9 ** 0.5) * 5)                   # 1s programm
+print(9.99 > 9.98 and 1000 != 1000.1)            # 2nd programm
+print(((2 * 2 + 2)) == ((2 * (2 +2))))              # 3rd programm
+print((int((float('123.456')) * 10) % 10))    # 4rh programm
