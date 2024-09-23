aee9c286f8b0f2e71e1a4a92d56ba07a4ed92f93
aruna-makaruna14 <arun4ik14@gmail.com>
2024-09-23 19:21:14 +0300

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,5 @@
+curses = 'Курс "Python",'
+homework_count = 12
+time = 1.5
+task_time = 1.5 / 12
+print(curses, 'всего задач:', homework_count,"затрачено часов:", time, 'среднее время выполнения', task_time, 'часа.')
