a37046515af10f8932d00f13ccd3094424a2e36e
aruna-makaruna14 <arun4ik14@gmail.com>
2024-09-23 19:29:16 +0300

    Update переменные.py

--- переменные.py
+++ переменные.py
@@ -1,5 +1,5 @@
 curses = 'Курс "Python",'
 homework_count = 12
 time = 1.5
-task_time = 1.5 / 12
+task_time = time / homework_count
 print(curses, 'всего задач:', homework_count,"затрачено часов:", time, 'среднее время выполнения', task_time, 'часа.')
