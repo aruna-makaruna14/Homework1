027cd30223c19a4ee9ea3b41ed5d8d4d9a48303a
aruna-makaruna14 <arun4ik14@gmail.com>
2024-09-24 19:56:55 +0300

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,7 @@ name = 'Aruna'
 print('Name:', name)
 age = 33
 print ('Age:', age)
-new_age = age +3
-print('New_age:', new_age)
+age = age +3
+print('New_age:', age)
 is_student = True
 print('Is student:', True)
