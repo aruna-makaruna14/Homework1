06f04ae9dec71ef146182ba362d698e8b69d802d
aruna-makaruna14 <arun4ik14@gmail.com>
2024-09-26 17:48:02 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+from dataclasses import replace
+
+my_string = input('Where are you from?  ')
+print(my_string .upper())
+print(my_string .upper().lower())
+print(my_string[0])
+print(my_string[-1])
+print(my_string .replace(' ',''))
