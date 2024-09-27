3aee58f27c634facb35b7fdbf517404e8502427a
aruna-makaruna14 <arun4ik14@gmail.com>
2024-09-27 19:09:58 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,14 @@
+my_dict = {'Elena' :1978, 'Mariya' :1985, 'Evgeniy' :1991}
+print(my_dict)
+print(my_dict.get('Elena'))
+print(my_dict.get('Ilona'))
+my_dict.update({'Mark' :1652, 'Matvey' :1988})
+del my_dict['Elena']
+print(my_dict)
+
+my_set = {5, 8, 8, 13, 5, 14, 13, 6}
+print(my_set)
+my_set.add(33)
+my_set.add(48)
+my_set.discard(8)
+print(my_set)
