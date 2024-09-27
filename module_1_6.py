5039f3bfd7fe67f29cb1c5cb2d772481f4b58a6c
aruna-makaruna14 <arun4ik14@gmail.com>
2024-09-27 19:16:36 +0300

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -6,9 +6,9 @@ my_dict.update({'Mark' :1652, 'Matvey' :1988})
 del my_dict['Elena']
 print(my_dict)
 
-my_set = {5, 8, 8, 13, 5, 14, 13, 6}
+my_set = {5, 8, 8, 'red', (5, 14, 13)}
 print(my_set)
 my_set.add(33)
-my_set.add(48)
+my_set.add(True)
 my_set.discard(8)
 print(my_set)
